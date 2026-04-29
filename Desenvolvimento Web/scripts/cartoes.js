403b52c5197187ce198bc8f02c76dbeb5fc86217
Matheus Raiano <123113785+matheusraiano@users.noreply.github.com>
2026-04-28 21:17:49 -0300

    Add files via upload

---
+++ Desenvolvimento Web/scripts/cartoes.js
@@ -0,0 +1,20 @@
+let numeracao = 1
+
+function cartao() {
+    let cartao = document.querySelector("div.cartao")
+    let direito = document.querySelector("div.direito")
+
+    if (Number(numeracao) == 1) {
+        numeracao ++
+        cartao.style.backgroundImage = 'url("../midias/cartao_interlagos.png")'
+        direito.innerHTML = `<p>CARTÃO <br>INTERLAGOS</p>`
+    } else if (Number(numeracao) == 2) {
+        numeracao ++
+        cartao.style.backgroundImage = 'url("../midias/cartao2.png")'
+        direito.innerHTML = `<p>CARTÃO <br>MÔNACO</p>`
+    } else if (Number(numeracao) == 3) {
+        numeracao = 1
+        cartao.style.backgroundImage = 'url("../midias/cartao_classic.png")'
+        direito.innerHTML = `<p>CARTÃO <br>CLASSIC</p>`
+    }
+}
