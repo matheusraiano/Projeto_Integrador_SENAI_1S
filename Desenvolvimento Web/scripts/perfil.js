c2bb0f99b36ef67254d0a4663fa5564ba964e2f2
metelrzx <mathrzoff@gmail.com>
2026-05-08 15:28:08 -0300

    conta perfil js

--- Desenvolvimento Web/scripts/perfil.js
+++ Desenvolvimento Web/scripts/perfil.js
@@ -1,4 +1,5 @@
 //Código de Matheus Raiano
+import { inicializarConta } from '../scripts/pages/conta.js';
 //
 // ELEMENTOS PRINCIPAIS
 //
@@ -113,65 +114,6 @@ navItems.forEach(item => {
         item.classList.remove('ativo');
     }
 });
-//
-// ================================
-// CONTA
-// ================================
-//
-function inicializarConta() {
-    const horas = document.querySelector('.horas');
-
-    if (!horas) return;
-
-    atualizarHorario();
-
-    const relogioInterval = setInterval(() => {
-
-        atualizarHorario();
-    }, 1000);
-    // cleanup
-    cleanupFunctions.push(() => {
-
-        clearInterval(relogioInterval);
-    });
-}
-//
-// RELÓGIO
-//
-function atualizarHorario() {
-    const horas = document.querySelector('.horas');
-
-    if (!horas) return;
-
-    const data = new Date();
-
-    const diasSemana = [
-        'Domingo',
-        'Segunda-Feira',
-        'Terça-Feira',
-        'Quarta-Feira',
-        'Quinta-Feira',
-        'Sexta-Feira',
-        'Sábado'
-    ];
-
-    const hora = String(data.getHours()).padStart(2, '0');
-    const minuto = String(data.getMinutes()).padStart(2, '0');
-    const segundo = String(data.getSeconds()).padStart(2, '0');
-
-    const dia = String(data.getDate()).padStart(2, '0');
-    const mes = String(data.getMonth() + 1).padStart(2, '0');
-
-    const ano = data.getFullYear();
-
-    horas.innerHTML = `
-        ${hora}:${minuto}:${segundo}
-        -
-        ${dia}/${mes}/${ano}
-        -
-        ${diasSemana[data.getDay()]}
-    `;
-}
 //
 // ================================
 // CARTÕES
